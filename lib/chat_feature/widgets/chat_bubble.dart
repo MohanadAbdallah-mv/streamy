0991fe531ea6c82d0b783f6d10bbd586635d3f10
MohanadAbdallah-mv <mohanad1967@yahoo.com>
2024-05-09 10:21:42 +0300

    chatBubble icon added

--- lib/chat_feature/widgets/chat_bubble.dart
+++ lib/chat_feature/widgets/chat_bubble.dart
@@ -9,6 +9,7 @@ import '../model/Message.dart';
 class ChatBubble extends StatefulWidget {
   bool isSender;
   MyMessage message;
+
   ChatBubble({super.key, required this.isSender, required this.message});
 
   @override
@@ -28,49 +29,86 @@ class _ChatBubbleState extends State<ChatBubble> {
             widget.isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 5.h, bottom: 5.h),
+            padding: EdgeInsets.only(top: 5.h, bottom: 5.h,left: widget.isSender?0:20),
             child: Text(message.name ?? message.senderEmail),
           ),
-          Container(
-            margin: widget.isSender
-                ? EdgeInsets.only(right: 4, left: 16)
-                : EdgeInsets.only(right: 16, left: 4),
-            padding: EdgeInsets.all(12.h),
-            decoration: BoxDecoration(
-              borderRadius: widget.isSender
-                  ? BorderRadius.only(
-                      bottomLeft: Radius.circular(16.r),
-                      topLeft: Radius.circular(16.r),
-                      topRight: Radius.circular(16.r))
-                  : BorderRadius.only(
-                      bottomRight: Radius.circular(16.r),
-                      topLeft: Radius.circular(16.r),
-                      topRight: Radius.circular(16.r)),
-              color:
-                  widget.isSender ? mainBlueColor : messageReceiverColor,
-            ),
-            child: Text(
-              message.message,
-              style: TextStyle(
-                fontSize: 16.sp,
-                color: Colors.white,
+          widget.isSender
+              ? Container(
+                  margin: widget.isSender
+                      ? EdgeInsets.only(right: 4, left: 16)
+                      : EdgeInsets.only(right: 16, left: 4),
+                  padding: EdgeInsets.all(12.h),
+                  decoration: BoxDecoration(
+                    borderRadius: widget.isSender
+                        ? BorderRadius.only(
+                            bottomLeft: Radius.circular(16.r),
+                            topLeft: Radius.circular(16.r),
+                            topRight: Radius.circular(16.r))
+                        : BorderRadius.only(
+                            bottomRight: Radius.circular(16.r),
+                            topLeft: Radius.circular(16.r),
+                            topRight: Radius.circular(16.r)),
+                    color:
+                        widget.isSender ? mainBlueColor : messageReceiverColor,
+                  ),
+                  child: Text(
+                    message.message,
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              : Row(crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    radius: 10,
+                    child: Text(widget.message.recieverID.characters.first
+                        .toUpperCase()),
+                  ),
+                  Container(
+                    margin: widget.isSender
+                        ? EdgeInsets.only(right: 4, left: 16)
+                        : EdgeInsets.only(right: 16, left: 4),
+                    padding: EdgeInsets.all(12.h),
+                    decoration: BoxDecoration(
+                      borderRadius: widget.isSender
+                          ? BorderRadius.only(
+                              bottomLeft: Radius.circular(16.r),
+                              topLeft: Radius.circular(16.r),
+                              topRight: Radius.circular(16.r))
+                          : BorderRadius.only(
+                              bottomRight: Radius.circular(16.r),
+                              topLeft: Radius.circular(16.r),
+                              topRight: Radius.circular(16.r)),
+                      color: widget.isSender
+                          ? mainBlueColor
+                          : messageReceiverColor,
+                    ),
+                    child: Text(
+                      message.message,
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
           Padding(
             padding: widget.isSender
                 ? EdgeInsets.only(right: 8)
-                : EdgeInsets.only(left: 8),
+                : EdgeInsets.only(left: 20),
             child: Row(
               //mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisAlignment: widget.isSender
                   ? MainAxisAlignment.end
                   : MainAxisAlignment.start,
               children: [
-                Text(
-                  widget.isSender ? "read:${widget.message.read}" : "",
+                Icon(
+                  widget.isSender ? (widget.message.read!?Icons.check_circle:Icons.check_circle_outline) : null,
                   //textAlign: TextAlign.start,
-                ),
+                color: widget.isSender ? (widget.message.read!?Colors.blue:Colors.grey) : null,size: 20,),
                 Text(
                     DateFormat(' hh:mm-aa').format(message.timeStamp.toDate())),
               ],
