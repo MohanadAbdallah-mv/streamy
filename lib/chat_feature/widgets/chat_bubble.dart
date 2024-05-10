a5ddedfec35e4ad5b64c991b2e74982bc591877f
MohanadAbdallah-mv <mohanad1967@yahoo.com>
2024-05-10 16:19:18 +0300

    receiver text overflow wop

--- lib/chat_feature/widgets/chat_bubble.dart
+++ lib/chat_feature/widgets/chat_bubble.dart
@@ -29,7 +29,8 @@ class _ChatBubbleState extends State<ChatBubble> {
             widget.isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 5.h, bottom: 5.h,left: widget.isSender?0:20),
+            padding: EdgeInsets.only(
+                top: 5.h, bottom: 5.h, left: widget.isSender ? 0 : 20),
             //child: Text(message.name ?? message.senderEmail),
           ),
           widget.isSender
@@ -59,44 +60,45 @@ class _ChatBubbleState extends State<ChatBubble> {
                     ),
                   ),
                 )
-              : Row(crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  CircleAvatar(
-                    radius: 10,
-                    child: Text(message.name == null
-                        ? message.senderEmail.characters.first.toUpperCase()
-                        : message.name!.characters.first
-                        .toUpperCase()),
-                  ),
-                  Container(
-                    margin: widget.isSender
-                        ? EdgeInsets.only(right: 4, left: 16)
-                        : EdgeInsets.only(right: 16, left: 4),
-                    padding: EdgeInsets.all(12.h),
-                    decoration: BoxDecoration(
-                      borderRadius: widget.isSender
-                          ? BorderRadius.only(
-                              bottomLeft: Radius.circular(16.r),
-                              topLeft: Radius.circular(16.r),
-                              topRight: Radius.circular(16.r))
-                          : BorderRadius.only(
-                              bottomRight: Radius.circular(16.r),
-                              topLeft: Radius.circular(16.r),
-                              topRight: Radius.circular(16.r)),
-                      color: widget.isSender
-                          ? mainBlueColor
-                          : messageReceiverColor,
+              : Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      radius: 10,
+                      child: Text(message.name == null
+                          ? message.senderEmail.characters.first.toUpperCase()
+                          : message.name!.characters.first.toUpperCase()),
                     ),
-                    child: Text(
-                      message.message,
-                      style: TextStyle(
-                        fontSize: 16.sp,
-                        color: Colors.white,
+                    Container(
+                      margin: widget.isSender
+                          ? EdgeInsets.only(right: 4, left: 16)
+                          : EdgeInsets.only(right: 16, left: 4),
+                      padding: EdgeInsets.all(12.h),
+                      decoration: BoxDecoration(
+                        borderRadius: widget.isSender
+                            ? BorderRadius.only(
+                                bottomLeft: Radius.circular(16.r),
+                                topLeft: Radius.circular(16.r),
+                                topRight: Radius.circular(16.r))
+                            : BorderRadius.only(
+                                bottomRight: Radius.circular(16.r),
+                                topLeft: Radius.circular(16.r),
+                                topRight: Radius.circular(16.r)),
+                        color: widget.isSender
+                            ? mainBlueColor
+                            : messageReceiverColor,
+                      ),
+                      child: Text(
+                        //todo implement multiline handling here
+                        message.message,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
           Padding(
             padding: widget.isSender
                 ? EdgeInsets.only(right: 8)
@@ -108,9 +110,17 @@ class _ChatBubbleState extends State<ChatBubble> {
                   : MainAxisAlignment.start,
               children: [
                 Icon(
-                  widget.isSender ? (widget.message.read!?Icons.check_circle:Icons.check_circle_outline) : null,
+                  widget.isSender
+                      ? (widget.message.read!
+                          ? Icons.check_circle
+                          : Icons.check_circle_outline)
+                      : null,
                   //textAlign: TextAlign.start,
-                color: widget.isSender ? (widget.message.read!?Colors.blue:Colors.grey) : null,size: 20,),
+                  color: widget.isSender
+                      ? (widget.message.read! ? Colors.blue : Colors.grey)
+                      : null,
+                  size: 20,
+                ),
                 Text(
                     DateFormat(' hh:mm-aa').format(message.timeStamp.toDate())),
               ],
